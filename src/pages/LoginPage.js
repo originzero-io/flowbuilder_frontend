1ea9ab5ddf82dafe1668538dc14d18dd3cf0b8f3
DESKTOP-5EKLP3J\akins <sibayakin@gmail.com>
2021-06-28 14:20:03 +0300

    login page style

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Form, FormGroup, Label, Input } from "reactstrap";
 import { Redirect } from "react-router-dom";
 import { loginError, loginSuccess } from "../store/actions/authActions";
 import { checkAPI, postAPI } from "../services/authService";
@@ -12,9 +12,37 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
+  background: #11998e;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #38ef7d, #11998e);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #38ef7d, #11998e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
+const FormWrapper = styled.div`
+  width: 25%;
+  transform: scale(1.1);
+  padding: 25px;
+  border-radius: 4px;
+  background:rgba(47, 54, 64,0.8);
+  color:whitesmoke;
+  box-shadow: 0px 0px 11px 0px rgba(0,0,0,0.75);
+`;
+const Text = styled.div`
+  font-size:2.5vmin;
+  border-bottom:1px solid whitesmoke;
+  margin-bottom: 20px;
+`;
+const LoginButton = styled.button`
+  border:none;
+  border-radius: 8px;
+  padding: 5px;
+  float:right;
+  width:100%;
+  color:whitesmoke;
+  background: #11998e;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #38ef7d, #11998e);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #38ef7d, #11998e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 `;
 const ErrorMessage = styled.p`
-  color: red;
+  color: tomato;
 `;
 export default function LoginPage() {
   const dispatch = useDispatch();
@@ -55,52 +83,48 @@ export default function LoginPage() {
   }, []);
   return (
     <Container>
-      <Form
-        onSubmit={(e) => e.preventDefault()}
-        style={{
-          width: "25%",
-          border: "1px dashed green",
-          transform: 'scale(1.1)',
-          padding: "25px",
-          borderRadius: "4px",
-        }}
-      >
-        <FormGroup>
-          <Label>User Name</Label>
-          <Input
-            {...register("username", {
-              required: "Username must enter the field",
-            })}
-          />
-          {errors.username && (
-            <ErrorMessage>{errors.username.message}</ErrorMessage>
+      <FormWrapper>
+        <Text>Login</Text>
+        <Form onSubmit={(e) => e.preventDefault()}>
+          <FormGroup>
+            <Label>User Name</Label>
+            <Input
+              {...register("username", {
+                required: "Username must enter the field",
+              })}
+              placeholder="Enter username"
+            />
+            {errors.username && (
+              <ErrorMessage>{errors.username.message}</ErrorMessage>
+            )}
+          </FormGroup>
+          <FormGroup>
+            <Label>Password</Label>
+            <Input
+              type="password"
+              {...register("password", {
+                required: "Password must enter the field",
+                minLength: {
+                  value: 4,
+                  message: "Password must have at least 4 characters",
+                },
+              })}
+              placeholder="Enter password"
+            />
+            {errors.password && (
+              <ErrorMessage>{errors.password.message}</ErrorMessage>
+            )}
+          </FormGroup>
+          <LoginButton type="submit" onClick={handleSubmit(onSubmitHandle)}>
+            Login
+          </LoginButton>
+          {isAuthenticated ? (
+            <Redirect to="/panel" />
+          ) : (
+            <ErrorMessage>{errorMessage}</ErrorMessage>
           )}
-        </FormGroup>
-        <FormGroup>
-          <Label>Password</Label>
-          <Input
-            type="password"
-            {...register("password", {
-              required: "Password must enter the field",
-              minLength: {
-                value: 4,
-                message: "Password must have at least 4 characters",
-              },
-            })}
-          />
-          {errors.password && (
-            <ErrorMessage>{errors.password.message}</ErrorMessage>
-          )}
-        </FormGroup>
-        <Button type="submit" onClick={handleSubmit(onSubmitHandle)}>
-          Login
-        </Button>
-        {isAuthenticated ? (
-          <Redirect to="/panel" />
-        ) : (
-          <ErrorMessage>{errorMessage}</ErrorMessage>
-        )}
-      </Form>
+        </Form>
+      </FormWrapper>
     </Container>
   );
 }
