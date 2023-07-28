e8eb471e1eed298147b268d10969de22d64c3ca3
Akın Şibay <sibayakin@gmail.com>
2023-07-28 16:29:29 +0300

    feat: notification bottom-right

--- src/components/Shared/Notification/ToastNotification.jsx
+++ src/components/Shared/Notification/ToastNotification.jsx
@@ -4,7 +4,7 @@ import { Toaster } from "react-hot-toast";
 function ToastNotification() {
   return (
     <Toaster
-      position="top-right"
+      position="bottom-right"
       reverseOrder={false}
       gutter={8}
       containerClassName=""
